62bc20a5a3b2edcb6831954a140045af8cccde40
Aaron Lambley <aaron.lambley@gmail.com>
2025-05-22 11:14:34 +0100

    Tidy app folder and install material UI dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// Change this font to Roboto
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* this is where you would add a Navbar */}
         {children}
+        {/* this is where you would add a footer */}
       </body>
     </html>
   );
