9d5fb059d3dab164d60cd16b0410cb4e4331f6d6
Aaron Lambley <aaron.lambley@gmail.com>
2025-06-02 10:04:06 +0100

    Remove gradients

--- app/components/gradients/index.tsx
+++
@@ -1,4 +0,0 @@
-import BackgroundLeft from "./backgroundLeft";
-import BackgroundRight from "./backgroundRight";
-
-export { BackgroundLeft, BackgroundRight };
--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,16 @@
 
 import * as Layout from "./components/layout/index";
 import * as Sections from "./components/sections/index";
-import * as Gradients from "./components/gradients/index";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <Layout.Navigation />
       <div id="section" className="relative isolate px-6 pt-14 lg:px-8 border-b border-gray-200">
-        <Gradients.BackgroundLeft />
         <Sections.Hero />
-        <Gradients.BackgroundRight />
       </div>
       <div id="section" className="relative isolate px-6 pt-14 lg:px-8 border-t border-gray-200">
-        <Gradients.BackgroundLeft />
         <Sections.Services />
-        <Gradients.BackgroundRight />
       </div>
     </div>
   );
