37a8119761c9604667fc7e745efb3b95d98e155b
Aaron Lambley <aaron.lambley@gmail.com>
2025-06-02 09:55:08 +0100

    Adjust full screen height styling

--- app/components/layout/nav.tsx
+++ app/components/layout/nav.tsx
@@ -14,7 +14,7 @@ export default function Navigation() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="absolute inset-x-0 top-0 z-50">
+    <header className="absolute inset-x-0 top-0 z-50 sticky">
       <nav
         aria-label="Global"
         className="flex items-center justify-between p-6 lg:px-8"
--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,12 @@ export default function Home() {
   return (
     <div className="bg-white">
       <Layout.Navigation />
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div id="section" className="relative isolate px-6 pt-14 lg:px-8 border-b border-gray-200">
         <Gradients.BackgroundLeft />
         <Sections.Hero />
         <Gradients.BackgroundRight />
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"></div>
       </div>
-      <div className="relative isolate px-6 pt-14 lg:px-8">
+      <div id="section" className="relative isolate px-6 pt-14 lg:px-8 border-t border-gray-200">
         <Gradients.BackgroundLeft />
         <Sections.Services />
         <Gradients.BackgroundRight />
