8cba503a538f73e034f346ad657d91e1f4f08e7d
Aaron Lambley <aaron.lambley@gmail.com>
2025-06-02 14:07:39 +0100

    Fix Home navigation link href to point to the root path

--- content/layout.ts
+++ content/layout.ts
@@ -2,7 +2,7 @@ import * as Global from "@/content/global";
 import { NavigationItem, LegalLink, FooterAddress } from "@/types/layout";
 
 const navigation: NavigationItem[] = [
-  { label: "Home", href: "", icon: true },
+  { label: "Home", href: "/", icon: true },
   { label: "About", href: "#about" },
   { label: "Services", href: "#services" },
   { label: "Testimonials", href: "#testimonials" },
